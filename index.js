2fc188bbe06595a0c2a846493ee9d1715d9aca26
shaikot07 <saifulislamshaikat007@gmail.com>
2023-11-27 23:34:15 +0600

    fixt article /user route

--- index.js
+++ index.js
@@ -42,14 +42,50 @@ async function run() {
 
 
 
+
+            // jwt related api 
+            app.post('/jwt', async (req, res) => {
+                  const user = req.body;
+                  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+                  res.send({ token })
+            })
+
+            // middlewares 
+            const verifyToken = (req, res, next) => {
+                  console.log('inside verify token', req.headers.authorization);
+                  if (!req.headers.authorization) {
+                        return res.status(401).send({ message: 'unauthorized access' });
+                  }
+                  const token = req.headers.authorization.split(' ')[1];
+                  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                        if (err) {
+                              return res.status(401).send({ message: 'unauthorized access' })
+                        }
+                        req.decoded = decoded;
+                        next();
+                  })
+            }
+
+            // use verify admin after verifyToken
+            const verifyAdmin = async (req, res, next) => {
+                  const email = req.decoded.email;
+                  const query = { email: email };
+                  const user = await userCollection.findOne(query);
+                  const isAdmin = user?.role === 'admin';
+                  if (!isAdmin) {
+                        return res.status(403).send({ message: 'forbidden access' });
+                  }
+                  next();
+            }
+
             // user relataded api 
-            app.get('/users', async (req, res) => {
+            app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
                   const result = await userCollection.find().toArray();
                   res.send(result)
             });
 
             // admin check kore dekhbe j admin ki na 
-            app.get('/users/admin/:email',  async (req, res) => {
+            app.get('/users/admin/:email', verifyToken, async (req, res) => {
                   const email = req.params.email;
 
                   if (email !== req.decoded.email) {
@@ -79,7 +115,7 @@ async function run() {
             })
 
             // adddmin bana nor jonno fild update 
-            app.patch('/users/admin/:id', async (req, res) => {
+            app.patch('/users/admin/:id', verifyToken, async (req, res) => {
                   const id = req.params.id;
                   const filter = { _id: new ObjectId(id) };
                   const updatedDoc = {
@@ -103,10 +139,12 @@ async function run() {
                   res.send(result)
             });
             // get data by email 
-            app.get('/article', async (req, res) => {
+            app.get('/article/user', async (req, res) => {
                   const email = req.query.email;
-                  const query = { email: email }
+                  // console.log("its a my card", email);
+                  const query = { articleAuthorEmail: email }
                   const result = await articleCollection.find(query).toArray();
+                  console.log(result);
                   res.send(result)
             });
 
